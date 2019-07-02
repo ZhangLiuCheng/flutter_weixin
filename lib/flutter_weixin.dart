a796bd63fd09c8f61c09b07291f59275f4969cab
zhangliucheng <414616504@qq.com>
2019-07-02 21:11:01 +0800

    update

--- lib/flutter_weixin.dart
+++ lib/flutter_weixin.dart
@@ -4,31 +4,46 @@ import 'dart:typed_data';
 import 'package:flutter/services.dart';
 
 class FlutterWeixin {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_weixin');
+    static const MethodChannel _channel =
+    const MethodChannel('flutter_weixin');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+    static Future<String> get platformVersion async {
+        final String version = await _channel.invokeMethod(
+            'getPlatformVersion');
+        return version;
+    }
 
-  static Future init(wxAppId) async {
-    return await _channel.invokeMethod("init", {"wxAppId" : wxAppId});
-  }
+    static Future init(wxAppId) async {
+        return await _channel.invokeMethod("init", {"wxAppId": wxAppId});
+    }
 
-  static Future shareToSession({String title, String description, String imgPath, String imgUrl, Uint8List imgData,
-    String webUrl, String webImgUrl, String webImgPath}) async {
+    static Future shareToSession({String title, String description, String imgPath, String imgUrl, Uint8List imgData,
+            String webUrl, String webImgUrl, String webImgPath}) async {
+        Map param = {
+            "title": title,
+            "description": description,
+            "imgPath": imgPath,
+            "imgUrl": imgUrl,
+            "imgData": imgData,
+            "webUrl": webUrl,
+            "webImgUrl": webImgUrl,
+            "webImgPath": webImgPath
+        };
+        return await _channel.invokeMethod('shareToSession', param);
+    }
 
-    Map param = {"title" : title, "description" : description, "imgPath": imgPath, "imgUrl": imgUrl, "imgData": imgData,
-      "webUrl" : webUrl, "webImgUrl" : webImgUrl, "webImgPath" : webImgPath};
-    return await _channel.invokeMethod('shareToSession', param);
-  }
-
-  static Future shareToTimeline({String title, String description, String imgPath, String imgUrl, Uint8List imgData,
-    String webUrl, String webImgUrl, String webImgPath}) async {
-
-    Map param = {"title" : title, "description" : description, "imgPath": imgPath, "imgUrl" : imgUrl, "imgData": imgData,
-      "webUrl" : webUrl, "webImgUrl" : webImgUrl, "webImgPath" : webImgPath};
-    return await _channel.invokeMethod('shareToTimeline', param);
-  }
+    static Future shareToTimeline({String title, String description, String imgPath, String imgUrl, Uint8List imgData,
+            String webUrl, String webImgUrl, String webImgPath}) async {
+        Map param = {
+            "title": title,
+            "description": description,
+            "imgPath": imgPath,
+            "imgUrl": imgUrl,
+            "imgData": imgData,
+            "webUrl": webUrl,
+            "webImgUrl": webImgUrl,
+            "webImgPath": webImgPath
+        };
+        return await _channel.invokeMethod('shareToTimeline', param);
+    }
 }
